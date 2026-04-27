a5170a1162341c7b424c9f1983ef20dbb1038dc1
LNAcodes <182744940+LNAcodes@users.noreply.github.com>
2026-04-27 17:48:56 +0200

    feat: add filter by tag, PATCH endpoint, and input validation

--- backend/express-basics/src/index.ts
+++ backend/express-basics/src/index.ts
@@ -28,31 +28,72 @@ let bookmarks: Bookmark[] = [
 app.use(express.json());
 
 app.get("/bookmarks", (req, res) => {
+  const tag = req.query.tag;
+
+  const filteredBookmarks = bookmarks.filter(
+    (bookmark) => bookmark.tag === tag,
+  );
+
+  if (tag) {
+    res.json(filteredBookmarks);
+    return;
+  }
   res.json(bookmarks);
 });
 
 app.get("/bookmarks/:id", (req, res) => {
   const id = Number(req.params.id);
-
   const bookmark = bookmarks.find((bookmark) => bookmark.id === id);
 
   if (!bookmark) {
     res.status(404).json({ message: `id ${id} not found.` });
     return;
   }
+
   res.json(bookmark);
 });
 
 app.post("/bookmarks", (req, res) => {
   const newBookmark = req.body;
+
+  if (!newBookmark.url) {
+    res.status(400).json({ error: `URL is missing` });
+    return;
+  }
+
+  if (!newBookmark.title) {
+    res.status(400).json({ error: `Title is missing` });
+    return;
+  }
+
   bookmarks.push({ ...newBookmark, id: bookmarks.length + 1 });
+
   res.status(201).json(newBookmark);
+  return;
 });
 
 app.delete("/bookmarks/:id", (req, res) => {
   const bookmarkId = Number(req.params.id);
   bookmarks = bookmarks.filter((bookmark) => bookmark.id !== bookmarkId);
   res.status(204).send();
+  return;
+});
+
+app.patch("/bookmarks/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const bookmark = bookmarks.find((bookmark) => bookmark.id === id);
+
+  if (!bookmark) {
+    res.status(404).json({ message: `id ${id} not found.` });
+    return;
+  }
+
+  const updatedBookmark = { ...bookmark, ...req.body };
+  bookmarks = bookmarks.map((bookmark) =>
+    bookmark.id === id ? updatedBookmark : bookmark,
+  );
+
+  res.status(200).json(updatedBookmark);
 });
 
 app.listen(port, () => {
