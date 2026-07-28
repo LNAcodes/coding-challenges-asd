7d6dbbcdec672eeca27396314b1649e8860c602e
LNAcodes <182744940+LNAcodes@users.noreply.github.com>
2026-07-28 10:45:19 +0200

    feat: sanitize and store message as UUID-named file

--- backend/backend-express-advanced/burn-on-read-service/src/routes/messages.ts
+++ backend/backend-express-advanced/burn-on-read-service/src/routes/messages.ts
@@ -1,11 +1,19 @@
 import { Router } from "express";
 import sanitize from "sanitize-html";
+import { writeFile } from "node:fs/promises";
+import path from "node:path";
 
 const router = Router();
 
-router.post("/messages", (req, res) => {
+router.post("/messages", async (req, res) => {
   const sanitizedMessage = sanitize(req.body.message);
   console.log(req.body);
+  const messageId = crypto.randomUUID();
+  console.log(messageId);
+
+  const filePath = path.join(process.cwd(), "messages", `${messageId}.txt`);
+  await writeFile(filePath, sanitizedMessage);
+
   res.send("ok");
 });
 
