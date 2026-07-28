045f00a4a1c57e2ec6ecd909fe01af7dcf017137
LNAcodes <182744940+LNAcodes@users.noreply.github.com>
2026-07-28 12:20:11 +0200

    feat: add GET messages:id route, detail.html njk template for one time reading secret mssg

--- backend/backend-express-advanced/burn-on-read-service/src/routes/messages.ts
+++ backend/backend-express-advanced/burn-on-read-service/src/routes/messages.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import sanitize from "sanitize-html";
 import { writeFile } from "node:fs/promises";
 import path from "node:path";
+import { readFile } from "node:fs/promises";
 
 const router = Router();
 
@@ -15,6 +16,18 @@ router.post("/messages", async (req, res) => {
   await writeFile(filePath, sanitizedMessage);
 
   res.render("success.html", { messageId });
+});
+
+router.get("/messages/:id", async (req, res) => {
+  const { id } = req.params;
+  // console.log("ID aus URL:", id);
+
+  const filePath = path.join(process.cwd(), "messages", `${id}.txt`);
+  // console.log("FilePath:", filePath);
+
+  const message = await readFile(filePath, { encoding: "utf-8" });
+  // console.log("Decoded message:", message);
+  res.render("detail.html", { message });
 });
 
 export default router;
