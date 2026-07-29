a5329eedc3b537aad73fb468e2704cd4684f9ddf
LNAcodes <182744940+LNAcodes@users.noreply.github.com>
2026-07-29 15:32:26 +0200

    refactor: move logger middleware to correct position in middleware chain

--- backend/backend-express-advanced/burn-on-read-service/src/index.ts
+++ backend/backend-express-advanced/burn-on-read-service/src/index.ts
@@ -12,6 +12,7 @@ const app = express();
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(logger);
 app.use(messagesRouter);
 
 const port = process.env.PORT || "3000";
@@ -27,7 +28,6 @@ app.get("/", (req, res) => {
 
 async function startServer() {
   await ensureLogFile(LOG_FILE);
-  app.use(logger);
   app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
   });
