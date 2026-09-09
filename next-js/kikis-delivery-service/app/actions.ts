4e32e3a553d42aff41d7961468f53562159cf792
LNAcodes <182744940+LNAcodes@users.noreply.github.com>
2026-09-09 11:53:42 +0200

    Remove redundant use server directive inside addDelivery

--- next-js/kikis-delivery-service/app/actions.ts
+++ next-js/kikis-delivery-service/app/actions.ts
@@ -4,7 +4,6 @@ import { createDelivery } from "@/lib/services/deliveriesService";
 import { revalidatePath } from "next/cache";
 
 export async function addDelivery(formData: FormData) {
-  "use server";
   const pickup = formData.get("pickup") as string;
   const destination = formData.get("destination") as string;
   await createDelivery({ pickup, destination });
